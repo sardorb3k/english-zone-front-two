0ae88029bbbedd012f5ab84a90f7a398b5fffa22
sardorb3k <s.sardorb3k@gmail.com>
2022-07-07 01:15:26 -0700

    BF

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-// import $ from "jquery";
-// import "swiper/css";
+
 import Header from "../components/Header/header";
-import { useEffect, useState } from "react";
 export default function Document() {
-  const [featured, setFeatured] = useState(null);
 
   return (
     <Html>
@@ -17,12 +13,9 @@ export default function Document() {
           <Main />
         </main>
         <NextScript />
-        {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script> */}
         <script src="static/js/vendor/jquery-3.5.1.min.js"></script>
         <script src="static/js/swiper-bundle.js"></script>
         <script src="static/js/ajax-form.js"></script>
-        {/* <script src="static/js/backToTop.js"></script> */}
         <script src="static/js/bootstrap.bundle.min.js"></script>
         <script src="static/js/imagesloaded.pkgd.min.js"></script>
         <script src="static/js/isotope.pkgd.min.js"></script>
