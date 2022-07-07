6569361fc61087b5f4356461d000d938f4eca27f
sardorb3k <s.sardorb3k@gmail.com>
2022-07-07 01:16:57 -0700

    BF

--- pages/_document.js
+++ pages/_document.js
@@ -13,18 +13,18 @@ export default function Document() {
           <Main />
         </main>
         <NextScript />
-        <script src="static/js/vendor/jquery-3.5.1.min.js"></script>
-        <script src="static/js/swiper-bundle.js"></script>
-        <script src="static/js/ajax-form.js"></script>
-        <script src="static/js/bootstrap.bundle.min.js"></script>
-        <script src="static/js/imagesloaded.pkgd.min.js"></script>
-        <script src="static/js/isotope.pkgd.min.js"></script>
-        <script src="static/js/jquery.counterup.min.js"></script>
-        <script src="static/js/jquery.fancybox.min.js"></script>
-        <script src="static/js/jquery.meanmenu.js"></script>
-        <script src="static/js/owl.carousel.min.js"></script>
-        <script src="static/js/parallax.min.js"></script>
-        <script src="static/js/wow.min.js"></script>
+        <script src="static/js/vendor/jquery-3.5.1.min.js" async />
+        <script src="static/js/swiper-bundle.js" async />
+        <script src="static/js/ajax-form.js" async />
+        <script src="static/js/bootstrap.bundle.min.js" async />
+        <script src="static/js/imagesloaded.pkgd.min.js" async />
+        <script src="static/js/isotope.pkgd.min.js" async />
+        <script src="static/js/jquery.counterup.min.js" async />
+        <script src="static/js/jquery.fancybox.min.js" async />
+        <script src="static/js/jquery.meanmenu.js" async />
+        <script src="static/js/owl.carousel.min.js" async />
+        <script src="static/js/parallax.min.js" async />
+        <script src="static/js/wow.min.js" async />
       </body>
     </Html>
   );
